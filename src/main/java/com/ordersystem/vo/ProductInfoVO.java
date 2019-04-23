d652e979b63fbaa362e505ab0924700be84e53eb
e <luohaonan520@gmail.com>
2019-04-23 14:05:19 +0800

    1

---
+++ src/main/java/com/ordersystem/vo/ProductInfoVO.java
@@ -0,0 +1,28 @@
+//create bu howard 
+package com.ordersystem.vo;
+
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+@Data
+public class ProductInfoVO{
+
+    @JsonProperty("id")
+    private String productId;
+
+    @JsonProperty("name")
+    private String productName;
+
+    @JsonProperty("price")
+    private BigDecimal productPrice;
+
+    @JsonProperty("description")
+    private String productDescription;
+
+    @JsonProperty("icon")
+    private String productIcon;
+
+}
