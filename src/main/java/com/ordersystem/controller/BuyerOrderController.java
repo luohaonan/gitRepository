c49bb2cc1a8d86d90fa39782e2abcfbed7a75e56
e <luohaonan520@gmail.com>
2019-05-08 23:54:31 +0800

    0507

--- src/main/java/com/ordersystem/controller/BuyerOrderController.java
+++ src/main/java/com/ordersystem/controller/BuyerOrderController.java
@@ -1,6 +1,8 @@
 //create by howard in 20190507
 package com.ordersystem.controller;
+
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.validation.Valid;
@@ -15,10 +17,15 @@ import com.ordersystem.utils.ResultVOUtil;
 import com.ordersystem.vo.ResultVO;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
@@ -50,9 +57,23 @@ public class BuyerOrderController{
         Map<String, String> map = new HashMap<>();
         map.put("orderId", createResult.getOrderId());       
         return ResultVOUtil.success(map);
-
     }
     //订单列表
+    @SuppressWarnings("unchecked")
+    @GetMapping("/list")
+    public ResultVO<List<OrderDTO>> list(@RequestParam("openid") String openid,
+                                         @RequestParam(value = "page", defaultValue = "0") Integer page,
+                                         @RequestParam(value = "size", defaultValue = "10") Integer size) {
+        if (StringUtils.isEmpty(openid)) {
+            log.error("【查询订单列表】openid为空");
+            throw new SellException(ResultEnum.PARAM_ERROR);
+        }
+
+        PageRequest request = PageRequest.of(page, size);
+        Page<OrderDTO> orderDTOPage = orderService.findList(openid, request);
+
+        return ResultVOUtil.success(orderDTOPage.getContent());
+    }
     //订单详情
     //取消订单
 
