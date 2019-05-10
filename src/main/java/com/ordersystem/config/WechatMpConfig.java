9104773c0125d1b9bdd19baf01d7afa81991d822
e <luohaonan520@gmail.com>
2019-05-10 21:57:16 +0800

    0510

---
+++ src/main/java/com/ordersystem/config/WechatMpConfig.java
@@ -0,0 +1,35 @@
+//create by howard in 20100510
+//微信公众帐号的配置
+package com.ordersystem.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import me.chanjar.weixin.mp.api.WxMpConfigStorage;
+import me.chanjar.weixin.mp.api.WxMpInMemoryConfigStorage;
+import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.api.impl.WxMpServiceImpl;
+
+
+@Component
+public class WechatMpConfig {
+
+    @Autowired
+    private WechatAccountConfig accountConfig;
+
+    @Bean
+    public WxMpService wxMpService() {
+        WxMpService wxMpService = new WxMpServiceImpl();
+        wxMpService.setWxMpConfigStorage(wxMpConfigStorage());
+        return wxMpService;
+    }
+
+    @Bean
+    public WxMpConfigStorage wxMpConfigStorage() {
+        WxMpInMemoryConfigStorage wxMpConfigStorage = new WxMpInMemoryConfigStorage();
+        wxMpConfigStorage.setAppId(accountConfig.getMpAppId());
+        wxMpConfigStorage.setSecret(accountConfig.getMpAppSecret());
+        return wxMpConfigStorage;
+    }
+}
